56c57ef508bb5711e9e0066eb498650c8ff262d8
Ilya Shakshin <shakshin98@gmail.com>
2022-12-12 21:28:39 +0300

    added form object and fix some problems

--- script.js
+++ script.js
@@ -1,43 +1,24 @@
-let formData = {};
+const STRING_FIELDS = ["firstname", "lastname"];
+const LOCAL_STORAGE_ACCOUNT_KEY = "account";
+const FORM_KEY = "form";
 
 const form = document.querySelector("form");
+const expenseElement = document.querySelector("#expense");
+const incomeElement = document.querySelector("#income");
+const totalElement = document.querySelector("#total");
+
 const LS = localStorage;
 
-// form.addEventListener("submit", function () {
-//   formData.income.push(1);
-// });
-
-// form.addEventListener("input", function (event) {
-//   formData[event.target.name] = event.target.value;
-//   LS.setItem("formData", JSON.stringify(formData));
-// });
-
-// if (LS.getItem("formData")) {
-//   formData = JSON.parse(LS.getItem("formData"));
-
-//   for (let key in formData) {
-//     form.elements[key].value = formData[key];
-//   }
-// }
-
-// console.log(form.getElementsByTagName("textarea")[0].value);
-// form.getElementsByTagName(
-//   "textarea"
-// )[0].value = `Имя: ${formData.name} Фамилия: ${formData.lastname}
-// Доходы: ${formData.income}
-// Расходы: ${formData.expense}
-// `;
-
-const account = {
-  name: "Илья",
-  surname: "Шакшин",
-  income: [55, 55],
-  expense: [44, 44],
+let account = {
+  firstname: "",
+  lastname: "",
+  income: [],
+  expense: [],
   totalIncome() {
-    return this.income.reduce((a, b) => a + b);
+    return this.income.reduce((acc, value) => acc + value, 0);
   },
   totalExpense() {
-    return this.expense.reduce((a, b) => a + b);
+    return this.expense.reduce((acc, value) => acc + value, 0);
   },
   accountInfo() {
     return JSON.stringify(account);
@@ -54,17 +35,66 @@ const account = {
 };
 
 form.addEventListener("input", function (event) {
-  console.log(event.target.valueAsNumber);
-});
+  const formValues = new FormData(form);
+  LS.setItem(FORM_KEY, formToObject(formValues));
 
-console.log(account);
+  if (!STRING_FIELDS.includes(event.target.name)) {
+    return;
+  }
 
-form.addEventListener("input", function (event) {
   account[event.target.name] = event.target.value;
-  LS.setItem("account", JSON.stringify(account));
+  LS.setItem(LOCAL_STORAGE_ACCOUNT_KEY, account.accountInfo());
+
+  showTotal();
 });
 
 form.addEventListener("submit", function (event) {
-  console.log(event.target.valueAsNumber);
   event.preventDefault();
+
+  const data = new FormData(form);
+  for (const [key, value] of data) {
+    if (value === "") {
+      continue;
+    }
+    if (key === "income") {
+      account.addIncome(Number(value));
+      incomeElement.value = "";
+    }
+    if (key === "expense") {
+      account.addExpense(Number(value));
+      expenseElement.value = "";
+    }
+  }
+  LS.setItem(LOCAL_STORAGE_ACCOUNT_KEY, account.accountInfo());
+  showTotal();
 });
+
+window.onload = function () {
+  const savedAccount = JSON.parse(LS.getItem(LOCAL_STORAGE_ACCOUNT_KEY));
+  if (savedAccount) {
+    account = { ...account, ...savedAccount };
+  }
+
+  const formData = JSON.parse(LS.getItem(FORM_KEY));
+  if (formData) {
+    for (let key in formData) {
+      form.elements[key].value = formData[key];
+    }
+  }
+
+  showTotal();
+};
+
+function showTotal() {
+  totalElement.innerHTML = `Имя: ${account.firstname} Фамилия: ${
+    account.lastname
+  }
+    Доходы: ${account.income}
+    Расходы: ${account.expense}
+    Баланс: ${account.accountBalance()}
+`;
+}
+
+function formToObject(form) {
+  return JSON.stringify(Object.fromEntries(form));
+}
